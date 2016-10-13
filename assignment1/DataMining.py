4643c4a91276d074a5ecb6ee4010a11bff7ebdbe
Joey <mf1633056@smail.nju.edu.cn>
2016-10-13 13:25:52 +0800

    first committ to upload my data mining's assignment1

---
+++ assignment1/DataMining.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import nltk
+from nltk import SnowballStemmer
+from nltk.corpus import stopwords
+import os
+import math
+
+rootDir = os.path.abspath('.') + '\\assignment1\\ICML'
+
+Stemmer = SnowballStemmer('english')
+stop_words = set(stopwords.words('english'))
+englishwords_set = set(nltk.corpus.words.words())
+allWords_set = set([])
+papers = {}
+word_idf = {}
+fileCounts = 0
+output_files = []
+
+for parent, dirnames, filenames in os.walk(rootDir):
+
+    for filename in filenames:
+
+        if parent == (rootDir + '\\7. Kernel Methods'):
+            if filename != 'desktop.ini':
+                output_files.append(filename)
+
+        fileCounts += 1
+
+        f = open(os.path.join(parent, filename), 'r', encoding='utf-8')
+        paper = f.read()
+
+        wordlist = nltk.word_tokenize(paper)
+
+        words_withoutStops = []
+
+        for word in wordlist:
+            if Stemmer.stem(word.lower()) in englishwords_set:
+                if word.lower() not in stop_words:
+                    words_withoutStops.append(Stemmer.stem(word.lower()))
+
+        papers[filename] = words_withoutStops
+        word_set = set(words_withoutStops)
+
+        for word in word_set:
+            if word in word_idf:
+                word_idf[word] += 1
+            else:
+                word_idf[word] = 1
+
+        allWords_set = allWords_set | word_set
+
+        f.close()
+
+for (word, idf) in word_idf.items():
+    word_idf[word] = math.log10(fileCounts/idf)
+
+allWords_list = list(allWords_set)
+allWords_list = sorted(allWords_list)
+allwords_file = open('assignment1\\allwords.txt', 'w+', encoding='utf-8')
+print(allWords_list, file=allwords_file)
+
+result_file = open('assignment1\\result.txt', 'w+', encoding='utf-8')
+
+for (filename, wordlist) in papers.items():
+
+    word_tf = {}
+    result = {}
+    wordCount = 0
+    for word in wordlist:
+        wordCount += 1
+        if word in word_tf:
+            word_tf[word] += 1
+        else:
+            word_tf[word] = 1
+
+    for (word, tf) in word_tf.items():
+        tf = tf / wordCount
+        result[word] = tf * word_idf[word]
+
+    if filename in set(output_files):
+        count = 0
+        print('[', end="", file=result_file)
+        for i in range(1, len(allWords_list)+1):
+            if allWords_list[i-1] in result:
+                count += 1
+                if count == len(result):
+                    print('%5d:%.10f' % (i, result[allWords_list[i-1]]), end="", file=result_file)
+                    break
+                print('%5d:%.10f,' % (i, result[allWords_list[i-1]]), end="", file=result_file)
+
+        print(']', file=result_file)
+
+allwords_file.close()
+result_file.close()
+
---
+++ assignment2/test.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+D=np.array([[0,411,213,219,296,397],
+            [411,0,204,203,120,152],
+            [213,204,0,73,136,245],
+            [219,203,73,0,90,191],
+            [296,120,136,90,0,109],
+            [ 397,152,245,191,109,0]])
+
+N = D.shape[0]
+T = np.zeros((N,N))
+
+# solution 1
+# ss = 1.0/N**2*np.sum(D**2)
+# for i in range(N):
+#    for j in range(i,N):
+#        T[i,j] = T[j,i] = -0.5*(D[i,j]**2 -1.0/N*np.dot(D[i,:],D[i,:]) -1.0/N*np.dot(D[:,j],D[:,j])+ss)
+
+
+
+#solution 2
+#K = np.dot(D,np.transpose(D))
+D2 = D**2
+H = np.eye(N) - 1/N
+T = -0.5*np.dot(np.dot(H,D2),H)
+
+eigVal,eigVec = np.linalg.eig(T)
+X = np.dot(eigVec[:,:2],np.diag(np.sqrt(eigVal[:2])))
+
+print('original distance','\tnew distance')
+for i in range(N):
+    for j in range(i+1,N):
+        print(np.str(D[i,j]),'\t\t',np.str("%.4f"%np.linalg.norm(X[i]-X[j])))
