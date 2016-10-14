aecc7f15b0071d5f171fe9bfe15b343f65d80ce9
Joey <mf1633056@smail.nju.edu.cn>
2016-10-14 13:32:32 +0800

    modify PCA

--- assignment2/DimensionalityReduction.py
+++ assignment2/DimensionalityReduction.py
@@ -50,58 +50,58 @@ def dimensionality_reduction(matrix, k):
     return np.transpose(eig_vec[:k, :])
 
 
+def accuracy_compute(matrix_train, matrix_test, labels_train, labels_test):
+
+    matched_label_count = 0
+    for i in range(matrix_test.shape[0]):
+        min_dist = 0
+        min_index = 0
+        for j in range(matrix_train.shape[0]):
+
+            dist = np.linalg.norm(matrix_test[i, :] - matrix_train[j, :])
+
+            if dist < min_dist:
+                min_dist = dist
+                min_index = j
+
+        if labels_test[i] == labels_train[min_index]:
+            matched_label_count += 1
+
+    accuracy = matched_label_count / matrix_test.shape[0]
+    return accuracy
+
+
 # 把矩阵从txt提取出来并把最后一列标志位进行分离
 SonarTrainMatrix, SonarTrainLabels = load_matrix_from_txt('sonar-train.txt')
 SonarTestMatrix, SonarTestLabels = load_matrix_from_txt('sonar-test.txt')
-# SpliceTrainMatrix, SpliceTrainLabels = load_matrix_from_txt('splice-train.txt')
-# SpliceTestMatrix, SpliceTestLabels = load_matrix_from_txt('splice-test.txt')
-
-# print(SonarTrainMatrix)
-# print(SonarTestLabels)
-# print(SonarTestMatrix)
-# print(SonarTestLabels)
-# print(SpliceTrainMatrix)
-# print(SpliceTrainLabels)
-# print(SpliceTestMatrix)
-# print(SpliceTestLabels)
+SpliceTrainMatrix, SpliceTrainLabels = load_matrix_from_txt('splice-train.txt')
+SpliceTestMatrix, SpliceTestLabels = load_matrix_from_txt('splice-test.txt')
 
 
 # 把矩阵每一行的均值变为0
 SonarTrainMatrix = matrix_mean_zero(SonarTrainMatrix)
 # SonarTestMatrix = matrix_mean_zero(SonarTestMatrix)
-# SpliceTrainMatrix = matrix_mean_zero(SpliceTrainMatrix)
+SpliceTrainMatrix = matrix_mean_zero(SpliceTrainMatrix)
 # SpliceTestMatrix = matrix_mean_zero(SpliceTestMatrix)
 
 
 # 求矩阵的协方差矩阵
 SonarTrainCovMatrix = np.cov(np.transpose(SonarTrainMatrix))
 # SonarTestCovMatrix = np.cov(np.transpose(SonarTestMatrix))
-# SpliceTrainCovMatrix = np.cov(np.transpose(SpliceTrainMatrix))
+SpliceTrainCovMatrix = np.cov(np.transpose(SpliceTrainMatrix))
 # SpliceTestCovMatrix = np.cov(np.transpose(SpliceTestMatrix))
 
+Sonar_DRMatrix = dimensionality_reduction(SonarTrainCovMatrix, 10)
+Splice_DRMatrix = dimensionality_reduction(SpliceTrainCovMatrix, 10)
 
-DRMatrix = dimensionality_reduction(SonarTrainCovMatrix, 10)
+SonarTrainDRMatrix = np.dot(SonarTrainMatrix, Sonar_DRMatrix)
+SonarTestDRMatrix = np.dot(SonarTestMatrix, Sonar_DRMatrix)
+SpliceTrainDRMatrix = np.dot(SpliceTrainMatrix, Splice_DRMatrix)
+SpliceTestDRMatrix = np.dot(SpliceTestMatrix, Splice_DRMatrix)
 
-SonarTrainDRMatrix = np.dot(SonarTrainMatrix, DRMatrix)
-SonarTestDRMatrix = np.dot(SonarTestMatrix, DRMatrix)
+Sonar_accuracy = accuracy_compute(SonarTrainDRMatrix, SonarTestDRMatrix, SonarTrainLabels, SonarTestLabels)
+print(Sonar_accuracy)
+Splice_accuracy = accuracy_compute(SpliceTrainDRMatrix, SpliceTestDRMatrix, SpliceTrainLabels, SpliceTestLabels)
+print(Splice_accuracy)
 
-print(SonarTrainDRMatrix.shape)
-print(SonarTestDRMatrix.shape)
 
-min_dist = 0
-min_index = 0
-matched_label_count = 0
-for i in range(SonarTestDRMatrix.shape[0]):
-    for j in range(SonarTrainDRMatrix.shape[0]):
-
-        dist = np.linalg.norm(SonarTestDRMatrix[i, :]-SonarTrainDRMatrix[j, :])
-
-        if dist < min_dist:
-            min_dist = dist
-            min_index = j
-
-    if SonarTestLabels[i] == SonarTrainLabels[j]:
-        matched_label_count += 1
-
-accuracy = matched_label_count / SonarTestDRMatrix.shape[0]
-print(accuracy)
