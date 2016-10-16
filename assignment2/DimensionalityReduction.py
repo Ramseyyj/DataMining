3995a1923692ab4c9f5917aae46da6f5017b0d7a
Joey <mf1633056@smail.nju.edu.cn>
2016-10-16 22:11:03 +0800

    delete test.py and complete PCA,SVD,ISOMAP,but there are some mistakes need to correct

--- assignment2/DimensionalityReduction.py
+++ assignment2/DimensionalityReduction.py
@@ -1,6 +1,9 @@
 # dimensionality reduction
 import numpy as np
 import os
+from scipy.sparse import csgraph
+
+const_maxfloat = 1.7976931348623157e+308
 
 
 # 从txt文件中读取矩阵
@@ -41,19 +44,19 @@ def matrix_mean_zero(matrix):
 
 
 # 根据协方差矩阵得到降维矩阵
-def dimensionality_reduction(matrix, k):
+def dimension_reduction(matrix, k):
 
     eig_val, eig_vec = np.linalg.eig(matrix)
 
-    sorted_index = np.argsort(-eig_val, axis=0)
+    sorted_index = np.argsort(-eig_val, axis=1)
 
     eig_vec = eig_vec[sorted_index]
 
     return np.transpose(eig_vec[:k, :])
 
 
-# PCA 降维法
-def PCA_dimensionality_reduction(matrix_train, matrix_test, k):
+# PCA降维法
+def PCA_dimension_reduction(matrix_train, matrix_test, k):
 
     # 把矩阵每一行的均值变为0
     matrix_train = matrix_mean_zero(matrix_train)
@@ -62,7 +65,7 @@ def PCA_dimensionality_reduction(matrix_train, matrix_test, k):
     cov_matrix = np.cov(np.transpose(matrix_train))
 
     # 对协方差矩阵进行降维
-    DR_matrix = dimensionality_reduction(cov_matrix, k)
+    DR_matrix = dimension_reduction(cov_matrix, k)
 
     # 用训练集得到的降维矩阵对训练集和测试集进行降维
     DR_matrix_train = np.dot(matrix_train, DR_matrix)
@@ -71,12 +74,70 @@ def PCA_dimensionality_reduction(matrix_train, matrix_test, k):
     return DR_matrix_train, DR_matrix_test
 
 
+# SVD降维法
+def SVD_dimension_reduction(matrix_train, matrix_test, k):
+
+    u, s, v = np.linalg.svd(matrix_train.T)
+
+    u = u[:, :k]
+
+    DR_matrix_trian = np.dot(matrix_train, u)
+    DR_matrix_test = np.dot(matrix_test, u)
+
+    return DR_matrix_trian, DR_matrix_test
+
+
+def KNN_algorithm(matrix, k_nn):
+
+    sorted_index = np.argsort(matrix, axis=1)
+
+    for i in range(matrix.shape[0]):
+
+        k_min_index_set = set(sorted_index[i, :k_nn+1])
+
+        for j in range(matrix.shape[1]):
+            if j not in k_min_index_set:
+                matrix[i, j] = 0.
+
+    return matrix
+
+
+def MDS_dimension_reduction(matrix, k):
+
+    M = matrix.shape[0]
+
+    matrix **= 2
+    H = np.eye(M) - 1 / M
+    B = -0.5 * np.dot(np.dot(H, matrix), H)
+
+    eigVal, eigVec = np.linalg.eig(B)
+    matrix = np.dot(eigVec[:, :k], np.diag(np.sqrt(eigVal[:k])))
+
+    return matrix
+
+
+def ISOMAP_dimension_reduction(matrix, k):
+
+    graph = np.zeros((matrix.shape[0], matrix.shape[0]))
+
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[0]):
+            graph[i, j] = np.linalg.norm(matrix[i, :] - matrix[j, :])
+
+    graph = KNN_algorithm(graph, 21)
+    graph = csgraph.shortest_path(graph, 'D', )
+
+    matrix = MDS_dimension_reduction(graph, k)
+
+    return matrix
+
+
 # 判断精度计算
 def accuracy_compute(matrix_train, matrix_test, labels_train, labels_test):
 
     matched_label_count = 0
     for i in range(matrix_test.shape[0]):
-        min_dist = 0
+        min_dist = const_maxfloat
         min_index = 0
         for j in range(matrix_train.shape[0]):
 
@@ -92,17 +153,24 @@ def accuracy_compute(matrix_train, matrix_test, labels_train, labels_test):
     accuracy = matched_label_count / matrix_test.shape[0]
     return accuracy
 
+
 # 把矩阵从txt提取出来并把最后一列标志位进行分离
 SonarTrainMatrix, SonarTrainLabels = load_matrix_from_txt('sonar-train.txt')
 SonarTestMatrix, SonarTestLabels = load_matrix_from_txt('sonar-test.txt')
 SpliceTrainMatrix, SpliceTrainLabels = load_matrix_from_txt('splice-train.txt')
 SpliceTestMatrix, SpliceTestLabels = load_matrix_from_txt('splice-test.txt')
 
-SonarTrainDRMatrix, SonarTestDRMatrix = PCA_dimensionality_reduction(SonarTrainMatrix, SonarTestMatrix, 10)
-SpliceTrainDRMatrix, SpliceTestDRMatrix = PCA_dimensionality_reduction(SpliceTrainMatrix, SpliceTestMatrix, 10)
+# SonarTrainDRMatrix, SonarTestDRMatrix = PCA_dimension_reduction(SonarTrainMatrix, SonarTestMatrix, 10)
+# SpliceTrainDRMatrix, SpliceTestDRMatrix = PCA_dimension_reduction(SpliceTrainMatrix, SpliceTestMatrix, 10)
+SonarTrainDRMatrix, SonarTestDRMatrix = SVD_dimension_reduction(SonarTrainMatrix, SonarTestMatrix, 30)
+# SonarTrainDRMatrix = ISOMAP_dimension_reduction(SonarTrainMatrix, 10)
+# SonarTestDRMatrix = ISOMAP_dimension_reduction(SonarTestMatrix, 10)
+
+# print(SonarTrainDRMatrix)
+# print(SonarTestDRMatrix)
 
 Sonar_accuracy = accuracy_compute(SonarTrainDRMatrix, SonarTestDRMatrix, SonarTrainLabels, SonarTestLabels)
 print(Sonar_accuracy)
-Splice_accuracy = accuracy_compute(SpliceTrainDRMatrix, SpliceTestDRMatrix, SpliceTrainLabels, SpliceTestLabels)
-print(Splice_accuracy)
+# Splice_accuracy = accuracy_compute(SpliceTrainDRMatrix, SpliceTestDRMatrix, SpliceTrainLabels, SpliceTestLabels)
+# print(Splice_accuracy)
 
