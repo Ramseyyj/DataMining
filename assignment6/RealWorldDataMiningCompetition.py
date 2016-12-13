a407ac071a09e37b2ae8935c0e56aa72a649fc8c
Joey <mf1633056@smail.nju.edu.cn>
2016-12-13 14:54:55 +0800

    did some test

--- assignment6/RealWorldDataMiningCompetition.py
+++ assignment6/RealWorldDataMiningCompetition.py
@@ -3,6 +3,10 @@ import datetime
 import os
 
 
+train_file = 'train_ver2.csv'
+test_file = 'test_ver2.csv'
+
+
 def print_csv_in_lines(file, lines=0):
 
     with open(file, 'r', encoding='utf-8') as csvfile:
@@ -21,10 +25,13 @@ def print_csv_in_lines(file, lines=0):
 
 def reduce_dataset(origin_file, reduce_file, reduce_lines):
 
-    with open(origin_file, 'r', encoding='utf-8') as csvfile:
+    root_dir = os.path.abspath('.') + '\\data'
+    csv_file_dir = root_dir + '\\' + origin_file
+
+    with open(csv_file_dir, 'r', encoding='utf-8') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
 
-        with open(reduce_file, 'w', newline='\n') as csvwritefile:
+        with open(reduce_file, 'w', encoding='utf-8', newline='\n') as csvwritefile:
             csvwriter = csv.writer(csvwritefile, delimiter=',')
             testline = reduce_lines
             for row in spamreader:
@@ -32,3 +39,6 @@ def reduce_dataset(origin_file, reduce_file, reduce_lines):
                 testline -= 1
                 if testline == 0:
                     break
+
+reduce_dataset(train_file, 'train_temp', 100)
+print_csv_in_lines('train_temp', 10)
