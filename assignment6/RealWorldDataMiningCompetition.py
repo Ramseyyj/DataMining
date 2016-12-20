5b0762f78cf44824817e1d000d0eb44b845ebbb2
Joey <mf1633056@smail.nju.edu.cn>
2016-12-20 21:30:03 +0800

    finish most function

--- assignment6/RealWorldDataMiningCompetition.py
+++ assignment6/RealWorldDataMiningCompetition.py
@@ -40,5 +40,53 @@ def reduce_dataset(origin_file, reduce_file, reduce_lines):
                 if testline == 0:
                     break
 
-reduce_dataset(train_file, 'train_temp', 100)
-print_csv_in_lines('train_temp', 10)
+
+def run_some_test_on_data(file):
+
+    root_dir = os.path.abspath('.') + '\\data'
+    csv_file_dir = root_dir + '\\' + file
+
+    line_number_count = 0
+    customer_set = set()
+    zero_new_product_count = 0
+    one_new_product_count = 0
+    other_new_product_count = 0
+
+    with open(csv_file_dir, 'r', encoding='utf-8') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+
+        for row in spamreader:
+
+            line_number_count += 1
+            if line_number_count % 100000 == 0:
+                print(line_number_count / 100000)
+
+            settlement_date = row[0]
+            customer_code = row[1]
+
+            temp_count = 0
+            for i in range(24, 48, 1):
+                if row[i] == '1':
+                    temp_count += 1
+
+            # if temp_count == 0:
+            #     zero_new_product_count += 1
+            # elif temp_count == 1:
+            #     one_new_product_count += 1
+            # else:
+            #     other_new_product_count += 1
+
+            if settlement_date == '2015-06-28' and temp_count == 1:
+                customer_set.add(customer_code)
+
+    print('line_number_count: %d' % line_number_count)
+    print('customer_set count: %d' % len(customer_set))
+    print('zero_new_product_count: %d' % zero_new_product_count)
+    print('one_new_product_count: %d' % one_new_product_count)
+    print('other_new_product_count: %d' % other_new_product_count)
+
+# reduce_dataset(test_file, 'test_temp', 100)
+print_csv_in_lines('train_temp', 5)
+print_csv_in_lines('test_temp', 5)
+
+# run_some_test_on_data(train_file)
